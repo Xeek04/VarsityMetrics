978ddf42ecac755bdf940badadf81d9f9ffde67b
Sam Vandiver <samuelvandiver@gmail.com>
2025-01-15 04:07:26 -0600

    Constants, DB design, started models

---
+++ VarsityMetrics/DB Models/Player.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+namespace VarsityMetrics.DB_Models
+{
+    [Table("Player")]
+    public class Player
+    {
+        [PrimaryKey]
+        [NotNull]
+        [AutoIncrement]
+        [Column("Pk")]
+        public int Pk { get; set; }
+        [Column("fname")]
+        public String Fname { get; set; }
+        [Column("lname")]
+        public String Lname { get; set; }
+        [Column("position")]
+        public String Position { get; set; }
+        [Column("running_yards")]
+        public int RunningYards { get; set; }
+
+    }
+}
