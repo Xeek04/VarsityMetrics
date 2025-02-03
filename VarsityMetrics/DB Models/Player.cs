de820a62d10abb3e85efaf9544cb5b6dd781df90
Sam Vandiver <samuelvandiver@gmail.com>
2025-02-03 12:35:09 -0600

    Player.cs finished with constructor

--- VarsityMetrics/DB Models/Player.cs
+++ VarsityMetrics/DB Models/Player.cs
@@ -4,19 +4,24 @@ namespace VarsityMetrics.DB_Models
     [Table("Player")]
     public class Player
     {
-        [PrimaryKey]
-        [NotNull]
-        [AutoIncrement]
-        [Column("Pk")]
-        public int Pk { get; set; }
+        [PrimaryKey, AutoIncrement, Column("Pk")]
+        public int? Pk { get; set; }
         [Column("fname")]
-        public String Fname { get; set; }
+        public String? Fname { get; set; }
         [Column("lname")]
-        public String Lname { get; set; }
+        public String? Lname { get; set; }
         [Column("position")]
-        public String Position { get; set; }
-        [Column("running_yards")]
-        public int RunningYards { get; set; }
+        public String? Position { get; set; }
+        [Column("rushing_yards")]
+        public int? RushingYards { get; set; }
+        //TODO implement stats tracked
 
+        public Player(String? firstName, String? lastName, String? position, int? rushingYards)
+        {
+            Fname = firstName;
+            Lname = lastName;
+            Position = position;
+            RushingYards = rushingYards;
+        }
     }
 }
