0d3bc1ea2e72a397e5a13c87f48e21ef6003b1ba
Sam Vandiver <samuelvandiver@gmail.com>
2025-02-03 13:28:55 -0600

    Create Play.cs

---
+++ VarsityMetrics/DB Models/Play.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarsityMetrics.DB_Models
+{
+    [Table("Play")]
+    public class Play
+    {
+
+        [PrimaryKey, AutoIncrement, Column("Pk")]
+        public int Pk { get; set; }
+
+        [Column("play_name")]
+        public String? PlayName { get; set; }
+
+        [Column("imgsrc")]
+        public String? ImageSource { get; set; } // file location on computer
+    }
+}
