51a279d531200281f2c617bde325f34c42bb2a80
Dillan <dylanmichael001@gmail.com>
2025-02-04 20:48:33 -0600

    Adding automatic light and dark themes

---
+++ VarsityMetrics/SettingsPage.xaml.cs
@@ -0,0 +1,24 @@
+namespace VarsityMetrics;
+
+public partial class SettingsPage : ContentPage
+{
+	public SettingsPage()
+	{
+		InitializeComponent();
+	}
+
+    private void LightDark (object sender, EventArgs e)
+    {
+
+    }
+
+    private void ColorChange (object sender, EventArgs e)
+    {
+
+    }
+
+    private void TeamDecor (object sender, EventArgs e)
+    {
+
+    }
+}
