db249fa1fb70a108b0602a059ea0dce743634da3
Sam Vandiver <samuelvandiver@gmail.com>
2025-02-04 23:30:26 -0600

    Create Footage.cs

---
+++ VarsityMetrics/DB Models/Footage.cs
@@ -0,0 +1,26 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarsityMetrics.DB_Models
+{
+    [Table("Footage")]
+    public class Footage
+    {
+
+        [PrimaryKey, AutoIncrement, Column("Pk")]
+        public int Pk { get; set; }
+
+        [Column("url")]
+        public String? Url { get; set; }
+
+        [Indexed, Column("game_id")]
+        public int? GameId { get; set; }
+
+        [Indexed, Column("play_id")]
+        public int? Date { get; set; }
+    }
+}
