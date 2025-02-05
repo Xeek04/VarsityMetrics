e458220d3755ac40ed1870a28f985e85b5865935
Dillan <dylanmichael001@gmail.com>
2025-02-05 13:23:32 -0600

    fixed the team decor button but the picker doesn't work yet, images also don't change

--- VarsityMetrics/SettingsPage.xaml.cs
+++ VarsityMetrics/SettingsPage.xaml.cs
@@ -3,7 +3,9 @@ using System.Runtime.CompilerServices;
 namespace VarsityMetrics;
 public partial class SettingsPage : ContentPage
 {
-	public SettingsPage()
+
+    string thematic = "Ruston High";
+    public SettingsPage()
 	{
 		InitializeComponent();
 
@@ -21,26 +23,27 @@ public partial class SettingsPage : ContentPage
         }
 
     }
-
-    private void ColorChange (object sender, EventArgs e)
+    private void ColorChange(object sender, EventArgs e)
     {
 
     }
-
     private void TeamDecor (object sender, EventArgs e)
     {
-        string thematic = "Ruston High";
         if (thematic == "Ruston High")
         {
             thematic = "Saints";
+            // source = ImageSource.FromFile("dotnet_bot.png");
         }
         else if (thematic == "Saints")
         {
             thematic = "LSU";
+            //Source = ImageSource.FromResource("dotnet_bot.png");
         }
         else if(thematic == "LSU")
         {
             thematic = "Ruston High";
+            //Source = ImageSource.FromFile("dotnet_bot.png");
         }
+        whatever.Text = thematic; 
     }
 }
