c75b96509d00e19c3d3224338c390e9b58f22a0a
Sam Vandiver <samuelvandiver@gmail.com>
2025-02-05 03:13:15 -0600

    Real final touch

--- VarsityMetrics/DB Models/Roster.cs
+++ VarsityMetrics/DB Models/Roster.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace VarsityMetrics.DB_Models
 {
+    [Table("Roster")]
     public class Roster
     {
-        // Before inserting into Roster you need a game with matching game_id and a player with matching player_id
 
         [PrimaryKey, AutoIncrement, Column("Pk")]
         public int Pk { get; set; }
