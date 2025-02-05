8a0e032bb12266682ad31c1e7972e6329938fb9e
Xeek04 <101283100+Xeek04@users.noreply.github.com>
2025-02-05 13:22:15 -0600

    Expanded Stats and Roster Pages

--- VarsityMetrics/StatsPage.xaml.cs
+++ VarsityMetrics/StatsPage.xaml.cs
@@ -11,27 +11,32 @@ public partial class StatsPage : ContentPage
         addplayerstat.IsVisible = false;
         StatLine.IsVisible = true;
         statnav.IsVisible = true;
-        //back.IsVisible = true;
-        //confirm.IsVisible = true;
     }
     private void BackButton(object sender, EventArgs e)
     {
-        //back.IsVisible = false;
-        //confirm.IsVisible = false;
         statnav.IsVisible = false;
         StatLine.IsVisible = false;
         addplayerstat.IsVisible = true;
+        ClearLines();
     }
     private void ConfirmAdd(object sender, EventArgs e)
     {
-        //back.IsVisible = false;
-        //confirm.IsVisible = false;
         statnav.IsVisible = false;
         StatLine.IsVisible = false;
         addplayerstat.IsVisible = true;
+        ClearLines();
     }
     private void FindStats(object sender, EventArgs e)
     {
 
+    }
+    private void ClearLines()
+    {
+        player.Text = string.Empty;
+        comps.Text = string.Empty;
+        atts.Text = string.Empty;
+        passyds.Text = string.Empty;
+        passtds.Text = string.Empty;
+        ints.Text = string.Empty;
     }
 }
