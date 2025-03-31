846d6a2caad985f0652ca1a59574fdfdacca5463
Xeek04 <zach.browning04@gmail.com>
2025-03-31 12:58:32 -0500

    Added Models for GameLog and GameStats

--- VarsityMetrics/StatsPage.xaml.cs
+++ VarsityMetrics/StatsPage.xaml.cs
@@ -172,6 +172,7 @@ public partial class StatsPage : ContentPage
         addplayerstats.IsVisible = true;
         PassingView.IsVisible = false;
         RushingView.IsVisible = false;
+        RecView.IsVisible = false;
         ClearLines();
     }
 
