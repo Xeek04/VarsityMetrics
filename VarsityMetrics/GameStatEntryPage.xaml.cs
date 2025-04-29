4dfd7def857c83b3f53efcfd095e7f7e19609a42
Xeek04 <zach.browning04@gmail.com>
2025-04-29 11:09:24 -0500

    Small Progress

--- VarsityMetrics/GameStatEntryPage.xaml.cs
+++ VarsityMetrics/GameStatEntryPage.xaml.cs
@@ -7,10 +7,23 @@ public partial class GameStatEntryPage : ContentPage
 	public GameStatEntryPage()
 	{
         InitializeComponent();
+        PopulateGames();
 	}
 
     private void Back(object sender, EventArgs e)
     {
         Navigation.PopAsync();
+    }
+
+    private async void PopulateGames()
+    {
+        List<Gamelog> gamelog = await App.db.GetSchedule();
+        List<string> result = new List<string>();
+        foreach (Gamelog game in gamelog)
+        {
+            if (!game.StatsEntered) { result.Add(game.OppTeam); }
+        }
+        
+        games.ItemsSource = result;
     }
 }
